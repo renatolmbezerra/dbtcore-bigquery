7d84bc5a13f471c92959d287a41870b0d91d0daf
Renato Macedo <renatolmb@hotmail.com>
2025-10-14 21:59:41 -0300

    update

--- models/semantic/time_spine/time_spine.sql
+++ models/semantic/time_spine/time_spine.sql
@@ -1,14 +1,6 @@
-{{ config(
-    materialized='table'
-) }}
-
-WITH spine AS (
-    {{ dbt_utils.date_spine(
-        datepart="day",
-        start_date="'2018-01-01'",
-        end_date="'2030-12-31'"
-    ) }}
-)
 SELECT
-    date AS date_day
-FROM spine
+    DATE(day) AS date_day
+FROM
+    UNNEST(
+        GENERATE_DATE_ARRAY('2018-01-01', '2030-12-31', INTERVAL 1 DAY)
+    ) AS day
